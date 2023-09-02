bc8e8b64a9124c028a2d976d27fe5a920c5601d9
rajeshchauhan1426 <rc810609@gmail>
2023-09-02 19:58:24 +0530

    new

---
+++ Sets.js
@@ -0,0 +1,83 @@
+//Creating a Set
+// Using the Set constructor
+// Using the Set constructor
+// Using the Set constructor
+ //const mySet = new Set();
+// Using set literal notation
+ //const mySet = new Set([]);
+
+
+
+
+
+// Adding Elements to a Set
+// const mySet = new Set();
+
+// mySet.add(1);
+// mySet.add("Hello");
+// mySet.add(true);
+
+// Now mySet contains the values 1, "Hello", and true
+
+
+
+// Removing Elements from a Set
+// const mySet = new Set();
+
+// mySet.add(1);
+// mySet.add(2);
+
+// console.log(mySet.delete(1)); // true
+// console.log(mySet.delete(3)); // false
+
+
+
+
+
+// Getting the Size of a Set
+// const mySet = new Set();
+
+// mySet.add(1);
+// mySet.add(2);
+
+// console.log(mySet.size); // 2
+
+
+
+
+
+
+
+
+
+// Iterating Through a Set
+// const mySet = new Set();
+
+// mySet.add(1);
+// mySet.add(2);
+
+// Using for...of loop
+// for (const item of mySet) {
+//   console.log(item);
+// }
+
+// Using forEach() method
+// mySet.forEach((item) => {
+//   console.log(item);
+// });
+
+
+
+
+
+
+// Converting a Set to an Array
+// const mySet = new Set();
+
+// mySet.add(1);
+// mySet.add(2);
+
+// const myArray = Array.from(mySet); // or [...mySet]
+// console.log(myArray); // [1, 2]
+
+
