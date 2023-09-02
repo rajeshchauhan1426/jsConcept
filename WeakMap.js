8e099ce753f04ff7e627d98278dfe652b5448343
rajeshchauhan1426 <rc810609@gmail>
2023-09-02 19:57:27 +0530

    new commit

---
+++ WeakMap.js
@@ -0,0 +1,7 @@
+// A WeakMap in JavaScript is a built-in data structure introduced in ECMAScript 2015 (ES6) that allows you to create a map of key-value pairs where the keys are weakly referenced. This means that if there are no other references to a key in the WeakMap, the key and its corresponding value can be garbage collected.
+
+
+
+
+// Weak References:
+// The key objects in a WeakMap are held by weak references, which means that if there are no other references to a key object, it can be automatically garbage collected. This is different from regular Map where keys are held by strong references.
